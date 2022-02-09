1affe0721868289144f023701e2b04aa6b61e3a6
mitchell martinez <mitchell.martinez@gmail.com>
2022-02-08 22:48:06 -0800

    Project Updates to VR Multiplayer Paint Project

--- Multiplayer Paint/Assets/Scripts/PaintController.cs
+++ Multiplayer Paint/Assets/Scripts/PaintController.cs
@@ -8,6 +8,10 @@ public class PaintController : MonoBehaviour
     public FlexibleColorPicker flexColor;
     public GameObject brush;
     private Material material;
+    private LineRenderer brushTip;
+    private LineRenderer linerend;
+    private int points;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +22,25 @@ public class PaintController : MonoBehaviour
     void Update()
     {
         material.color = flexColor.color;
+        linerend.startColor = flexColor.color;
+        linerend.endColor = flexColor.color;
+        bool triggerValue;
+        var rightHandDevice = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevice);
+        if (rightHandDevice[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        {
+            linerend.SetVertexCount(points + 1);
+            linerend.SetPosition(points, brush.transform.position);
+            points++;
+        }
+    }
+
+    public void DrawLine()
+    {
+        GameObject line = new GameObject();
+        linerend = line.AddComponent<LineRenderer>();
+        linerend.startWidth = 0.1f;
+        linerend.endWidth = 0.1f;
+        points = 0;
     }
 }
