6e74686b8bd4b4c1340fa8c39ba90412edb5fbdf
mitchell martinez <mitchell.martinez@gmail.com>
2022-02-09 22:40:26 -0800

    2 color picking and painting stations are implemented.
    Unsure of multiplayer compatibility

--- Multiplayer Paint/Assets/Scripts/PaintController.cs
+++ Multiplayer Paint/Assets/Scripts/PaintController.cs
@@ -5,29 +5,30 @@ using UnityEngine;
 
 public class PaintController : MonoBehaviour
 {
-    public FlexibleColorPicker flexColor;
     public GameObject brush;
-    private Material material;
-    private LineRenderer brushTip;
     private LineRenderer linerend;
     private int points;
+    private UnityEngine.XR.InputDevice device;
+    private Color newColor = Color.red;
 
     // Start is called before the first frame update
     void Start()
     {
-        material = brush.GetComponent<Renderer>().material;
+        brush.GetComponent<Renderer>().material.SetColor("_Color", newColor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        material.color = flexColor.color;
-        linerend.startColor = flexColor.color;
-        linerend.endColor = flexColor.color;
+        brush.GetComponent<Renderer>().material.SetColor("_Color", newColor);
         bool triggerValue;
         var rightHandDevice = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevice);
-        if (rightHandDevice[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        if (rightHandDevice.Count == 1)
+        {
+            device = rightHandDevice[0];
+        }
+        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
         {
             linerend.SetVertexCount(points + 1);
             linerend.SetPosition(points, brush.transform.position);
@@ -39,8 +40,16 @@ public class PaintController : MonoBehaviour
     {
         GameObject line = new GameObject();
         linerend = line.AddComponent<LineRenderer>();
+        linerend.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+        linerend.startColor = newColor;
+        linerend.endColor = newColor;
         linerend.startWidth = 0.1f;
         linerend.endWidth = 0.1f;
         points = 0;
+    }
+
+    public void ChangeColor(Color selectedColor)
+    {
+        newColor = selectedColor;
     }
 }
